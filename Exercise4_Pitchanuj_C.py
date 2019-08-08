98f17add93dd6686eea237e58e611d30e6c507dd
Pitchanuj <pitcha.jira@gmail.com>
2019-08-08 13:22:45 +0700

    Added Score

---
+++ Exercise4_Pitchanuj_C.py
@@ -0,0 +1,10 @@
+FoundEng = 88
+GenBus = 42.5
+IntroCom = 95.5
+ComProgram = 85
+
+print("---- Your Score ----")
+print("Foundation English :", FoundEng)
+print("General Business :", GenBus)
+print("Introduction to Computer Systems :", IntroCom)
+print("Computer Programming :", ComProgram)
