d9b5a324e9fa5d5d5b5fd5794cfa754f5b5b370a
Pitchanuj <pitcha.jira@gmail.com>
2020-05-26 17:55:30 +0700

    Create Lecture 50_Pitchanuj_C.py

---
+++ Lecture 50_Pitchanuj_C.py
@@ -0,0 +1,13 @@
+def AddNum(x,y):
+    print(x+y)
+def SubtractNum(x,y):
+    print(x-y)
+def DivideNum(x,y):
+    print(x/y)
+def MultiplyNum(x,y):
+    print(x*y)
+
+AddNum(100,20)
+SubtractNum(100,20)
+DivideNum(100,20)
+MultiplyNum(100,20)
